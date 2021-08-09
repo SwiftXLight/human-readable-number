9984b819b5bd766aad505bd0f0c494bef55d84b8
Andriy Panchenko <72575014+SwiftXLight@users.noreply.github.com>
2021-08-09 16:01:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 
-module.exports =  function numberToEnglish(n) {
-        
-    units = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
-        
-    tens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+module.exports = function numberToEnglish(n) {
 
-	if (n === 0) return "zero";
-	if (n < 20) return units[n];
-	
-	if (n < 100)
-		return (
-			tens[parseInt(n / 10)] +
-            (n % 10 !== 0 ? " " : "") +
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (n === 0) return 'zero';
+    if (n < 20) return units[n];
+
+    if (n < 100)
+        return (
+            tens[parseInt(n / 10)] +
+            (n % 10 !== 0 ? ' ' : '') +
             units[n % 10]
         );
-		
+
     let nString = n.toString();
-	
-	if (nString[1] === "0" && nString[2] === "0")
-		return units[nString[0]] + " hundred";
+
+    if (nString[1] === '0' && nString[2] === '0')
+        return units[nString[0]] + ' hundred';
     else
         return (
             units[nString[0]] +
-            " hundred " +
+            ' hundred ' +
             numberToEnglish(parseInt(nString[1] + nString[2]))
         );
-        
+
 }
